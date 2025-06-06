1d4b8cf13ea776bfafcd0862236c728fc9211f78
Yahya Al Salmi <109773161+EllE961@users.noreply.github.com>
2025-06-06 20:05:52 -0300

    save

--- prodmanager-frontend/src/redux/actions.js
+++ prodmanager-frontend/src/redux/actions.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 import * as actionTypes from './actionTypes';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 // Create Product Actions
 export const createProductRequest = () => ({
