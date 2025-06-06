134989f152da1d5afd9ab8570742b8acfca3f208
Yahya Al Salmi <109773161+EllE961@users.noreply.github.com>
2025-06-06 20:36:48 -0300

    save

--- prodmanager-frontend/src/pages/Products.js
+++ prodmanager-frontend/src/pages/Products.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Container, Row, Col, Button, Card, Spinner, Alert } from 'react-bootstrap';
-import { toast } from 'react-toastify';
 
 // Redux Actions
 import { 
